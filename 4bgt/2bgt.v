a59b3c462609161f28445d71b8f50719dba1cf42
Ben Huang <benhua476@gmail.com>
2026-05-29 15:58:24 -0700

    4bgt project started

---
+++ 4bgt/2bgt.v
@@ -0,0 +1,8 @@
+module 2bgt
+    (
+        input wire [1:0] i0, i1,
+        output wire eq
+    );
+
+    assign 
+endmodule
---
+++ eq2.v
@@ -0,0 +1,12 @@
+module eq2
+    (
+        input wire [1:0] a, b,
+        output wire aeqb
+    );
+    wire e0, e1;
+    eq1 eq_bit0_unit (.i0(a[0]), .i1(b[0]), .eq(e0));
+    eq1 eq_bit1_unit (a[1], b[1], e1);
+
+    assign aeqb = e0 & e1;
+
+endmodule
